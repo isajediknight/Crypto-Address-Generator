4b111a7e6f814689d0d170280245fead325fc20a
isajediknight <luke@isajediknight.com>
2021-01-10 13:25:54 -0600

    [bitcoinaddresses] duplicated module and removed testnet BTC address creation

---
+++ modules/bitcoinaddress_mainnet/wallet.py
@@ -0,0 +1,24 @@
+#  Bitcoin Address  v0.1
+#  Copyright (c) 2020 - https://github.com/fortesp/bitcoinaddress
+#  This software is distributed under the terms of the MIT License.
+#  See the file 'LICENSE' in the root directory of the present distribution,
+#  or http://opensource.org/licenses/MIT.
+
+from . import Address
+from . import Key
+
+
+class Wallet:
+
+    def __init__(self, seed=None):
+        self.key = Key(seed)
+        self.address = Address(self.key)
+
+        self.key.generate()
+        self.address.generate()
+
+    def __str__(self):
+        return """%s\n%s""" % (self.key, self.address)
+    
+    def _get_the_three(self):
+        return self.key._get_private_key(),self.address._get_public_address_1(),self.address._get_public_address_3()
